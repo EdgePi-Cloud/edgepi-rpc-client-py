9637c49222191a566bba2d3c893f300e21737100
iaj2 <isaacjames124@yahoo.ca>
2023-07-27 00:26:02 +0100

    update led proto

--- client_services/led/client_led_service.py
+++ client_services/led/client_led_service.py
@@ -16,30 +16,30 @@ class ClientLEDService():
         self.service_stub = led_pb.LEDService_Stub(self.client_rpc_channel)
         self.rpc_controller = None
 
-    def turn_led_on(self, led_name: LEDPins):
+    def turn_led_on(self, led_pin: LEDPins):
         """turn_on led method for sdk led module"""
-        request = led_pb.LEDName(
-            led_name = led_name.value
+        request = led_pb.LEDPin(
+            led_pin = led_pin.value
         )
         # Call SDK method through rpc channel client
         response = self.service_stub.turn_led_on(self.rpc_controller,request)
 
         return response.content
 
-    def turn_led_off(self, led_name: LEDPins):
+    def turn_led_off(self, led_pin: LEDPins):
         """turn_off led method for sdk led module"""
-        request = led_pb.LEDName(
-            led_name = led_name.value
+        request = led_pb.LEDPin(
+            led_pin = led_pin.value
         )
         # Call SDK method through rpc channel client
         response = self.service_stub.turn_led_off(self.rpc_controller,request)
 
         return response.content
 
-    def toggle_led(self, led_name: LEDPins):
+    def toggle_led(self, led_pin: LEDPins):
         """toggle_led method for sdk led module"""
-        request = led_pb.LEDName(
-            led_name = led_name.value
+        request = led_pb.LEDPin(
+            led_pin = led_pin.value
         )
         # Call SDK method through rpc channel client
         response = self.service_stub.toggle_led(self.rpc_controller,request)
