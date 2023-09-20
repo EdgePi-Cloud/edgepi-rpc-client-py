ac93cd39b02c199b0921af265b9c0bb443962890
Josiah Kievit <71205057+josiah-tesfu@users.noreply.github.com>
2023-09-20 11:52:33 -0700

    Fix build and test issues.

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,22 +1,8 @@
-"""Fixtures for all integration tests"""
-import os
+"""Integration test setup for RPC Server"""
 import subprocess
 import pytest
 
-
-
 @pytest.fixture(scope="session", autouse=True)
-def setup_and_teardown_server():
-    """ Sets up a server for tests and shut it down when it is finished"""
-    # Run server
-    server_file = os.path.abspath(os.path.join(os.path.dirname(__file__),
-                                                "../..", "edgepi_rpc_server.py"))
-
-    with subprocess.Popen(
-        ['python', server_file, '-c','./edgepi_rpc_server.conf']
-    ) as server_process:
-        # Run testpy tests
-        yield
-        # Terminate server
-        server_process.terminate()
-        server_process.wait()
+def setup_server():
+    """Server Setup"""
+    subprocess.run(["bash", "./start_server.sh"], check=True)
