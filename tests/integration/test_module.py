6354523e19ebdd4d0735edf0cac640862393769b
Josiah Kievit <josiahtesfu@gmail.com>
2023-10-04 17:00:54 -0700

    Fix module error.

--- tests/integration/test_module.py
+++ tests/integration/test_module.py
@@ -1,9 +1,11 @@
-"""Client Build Integration Test"""
 import subprocess
 import os
+import pytest
+from tempfile import TemporaryDirectory
 
 def test_rpc_client_install():
     """Test client import"""
+    
     # Build the wheel
     subprocess.check_call(["pip3", "install", "build"])
     subprocess.check_call(["python", "-m", "build"])
@@ -12,10 +14,18 @@ def test_rpc_client_install():
     wheel_file = next((file for file in os.listdir("dist") if file.endswith(".whl")), None)
     assert wheel_file, "Wheel file not found"
 
-    # Install the wheel
-    subprocess.check_call(["pip3", "install", os.path.join("dist", wheel_file)])
+    with TemporaryDirectory() as tempdir:
+        # Install the wheel inside temp directory
+        subprocess.check_call(["pip3", "install", os.path.join("dist", wheel_file), "--target", tempdir])
 
-    # Check imports
-    # pylint: disable=import-outside-toplevel, unused-import
-    from edgepi_rpc_client.services.led.client_led_service import ClientLEDService
-    from edgepi_rpc_client.services.led.led_pb_enums import LEDPins
+        # Add the temporary directory to sys.path so that we can import from there
+        import sys
+        sys.path.insert(0, tempdir)
+
+        # Check imports
+        # pylint: disable=import-outside-toplevel, unused-import
+        from edgepi_rpc_client.services.led.client_led_service import ClientLEDService
+        from edgepi_rpc_client.services.led.led_pb_enums import LEDPins
+
+        # Clean up sys.path
+        sys.path.remove(tempdir)
